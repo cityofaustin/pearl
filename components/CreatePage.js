38a5419acbe2ade8b320cd0f24df5376cb04594f
Ben <benweatherman@gmail.com>
2018-05-17 18:30:02 -0500

    Made create page dialog slightly more functional
    You can click around and stuff gets saved

--- components/CreatePage.js
+++ components/CreatePage.js
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faListOl } from '@fortawesome/free-solid-svg-icons'
 import { faCalendar, faIdCard, faNewspaper } from '@fortawesome/free-regular-svg-icons'
 
+const MAX_TITLE_CHARS = 55;
+
 
 export default class CreatePage extends React.Component {
   constructor(props) {
@@ -18,7 +20,45 @@ export default class CreatePage extends React.Component {
         {name: 'Department', icon: faIdCard, description: 'Basic information about a department, including contact information.'},
         {name: 'News Release', icon: faNewspaper, description: 'News releases are time-boxed and are archived after they expire.'},
       ],
+      selectedPageType: '',
+      selectedTitle: '',
+      titleCharsRemaining: MAX_TITLE_CHARS,
+      selectedTopic: '',
     }
+
+    this.onPrevious = this.onPrevious.bind(this);
+    this.onSelectPageType = this.onSelectPageType.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onSaveTitle = this.onSaveTitle.bind(this);
+    this.onCreatePage = this.onCreatePage.bind(this);
+  }
+
+  componentWillReceiveProps() {
+    this.setState({activePage: 0});
+  }
+
+  onPrevious() {
+    const {activePage} = this.state;
+    this.setState({activePage: activePage - 1});
+  }
+
+  onSelectPageType(pageType) {
+    const {activePage} = this.state;
+    this.setState({selectedPageType: pageType.name, activePage: activePage + 1});
+  }
+
+  onTitleChange(e) {
+    const title = e.target.value;
+    this.setState({selectedTitle: title, titleCharsRemaining: MAX_TITLE_CHARS - title.length});
+  }
+
+  onSaveTitle() {
+    const {activePage} = this.state;
+    this.setState({activePage: activePage + 1});
+  }
+
+  onCreatePage() {
+    this.props.onClose();
   }
 
   renderPageTypeSelector() {
@@ -29,11 +69,11 @@ export default class CreatePage extends React.Component {
         <div className="container">
           {this.state.pageTypes.map((pageType, i) => {
             return (
-              <button key={i}>
-                <div class="card-body">
+              <button key={i} onClick={this.onSelectPageType.bind(this, pageType)}>
+                <div className="card-body">
                   <FontAwesomeIcon icon={pageType.icon} />
-                  <h5 class="text-center card-title">{ pageType.name }</h5>
-                  <p class="card-text">{ pageType.description }</p>
+                  <h5 className="text-center card-title">{ pageType.name }</h5>
+                  <p className="card-text">{ pageType.description }</p>
                 </div>
               </button>
             )
@@ -50,7 +90,7 @@ export default class CreatePage extends React.Component {
   }
 
   renderTitleInput() {
-    const titleCharsRemaining = 55;
+    const {selectedTitle, titleCharsRemaining} = this.state;
 
     return (
       <div>
@@ -58,11 +98,11 @@ export default class CreatePage extends React.Component {
         <h3>It should start with a verb!</h3>
 
         <form>
-          <div class="form-group">
-            <label for="pageTitleInput">Page Title</label>
-            <input class="form-control" v-model="title" id="pageTitleInput" aria-describedby="pageTitleHelp" placeholder="" />
-            <small id="pageTitleHelp" class="form-text text-muted">Example: Drop off household hazardous waste and other recyclables</small>
-            <small class="form-text text-muted">Psst! You can use up to { titleCharsRemaining } more characters</small>
+          <div className="form-group">
+            <label htmlFor="pageTitleInput">Page Title</label>
+            <input value={selectedTitle} onChange={this.onTitleChange} aria-describedby="pageTitleHelp" placeholder="" />
+            <small id="pageTitleHelp" className="form-text text-muted">Example: Drop off household hazardous waste and other recyclables</small>
+            <small className="form-text text-muted">Psst! You can use up to {titleCharsRemaining} more characters</small>
           </div>
         </form>
 
@@ -75,7 +115,15 @@ export default class CreatePage extends React.Component {
           </li>
         </ul>
 
+        <div className="button-group">
+          <button onClick={this.onPrevious}>Previous</button>
+          <button onClick={this.onSaveTitle}>Next</button>
+        </div>
+
         <style jsx>{`
+          .button-group {
+            display: flex;
+          }
         `}</style>
       </div>
     )
@@ -85,6 +133,11 @@ export default class CreatePage extends React.Component {
     return (
       <div>
         <h1>Please select the topic that best fits</h1>
+
+        <div className="button-group">
+          <button onClick={this.onPrevious}>Previous</button>
+          <button onClick={this.onCreatePage}>Create Page</button>
+        </div>
       </div>
     )
   }
