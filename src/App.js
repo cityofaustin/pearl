4522aefd0c46a58cb4b26ecdc74f1ee0a99fe342
Simi <simi.damani@austintexas.gov>
2018-06-13 17:25:16 -0500

    add prettier and autoformat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { ApolloProvider } from "react-apollo";
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
 
-import Pages from "./components/Pages";
-import "./App.css";
+import Pages from './components/Pages';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={this.props.client}>
         <div className="App">
-          <h2>OUR  first deployed Apollo app 🚀</h2>
+          <h2>OUR first deployed Apollo app 🚀</h2>
           <Pages />
         </div>
       </ApolloProvider>
--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,6 +1,6 @@
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
-import React from "react";
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import React from 'react';
 
 const GET_PAGES = gql`
   {
@@ -18,7 +18,7 @@ const GET_PAGES = gql`
 const Pages = () => (
   <Query query={GET_PAGES}>
     {({ loading, error, data }) => {
-      if (loading) return "Loading...";
+      if (loading) return 'Loading...';
       if (error) return `Error! ${error.message}`;
 
       return data.allServicePages.edges.map(edge => (
