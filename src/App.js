8cc64c85b7cf6a6a5a4104b99fd6321fb69d689e
Brian Smith <briaguya@gmail.com>
2018-06-13 15:32:43 -0500

    apollo loads data from local joplin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+
+import Pages from "./Pages";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={this.props.client}>
+        <div>
+          <h2>My first Apollo app 🚀</h2>
+          <Pages />
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/Pages.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import React from "react";
+
+const GET_PAGES = gql`
+  {
+    allServicePages {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
+
+const Pages = () => (
+  <Query query={GET_PAGES}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return data.allServicePages.edges.map(edge => (
+        <div key={edge.node.id} value={edge.node.title}>
+          {edge.node.title}
+        </div>
+      ));
+    }}
+  </Query>
+);
+
+export default Pages;
