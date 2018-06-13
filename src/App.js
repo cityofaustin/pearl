fee9d02f963ba46d2a550c1699887d20a1b9c832
Mateo Clarke <code@mateoclarke.com>
2018-06-13 15:55:56 -0500

    Add Sass support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
 
-import Pages from "./Pages";
+import Pages from "./components/Pages";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={this.props.client}>
-        <div>
+        <div className="App">
           <h2>My first Apollo app 🚀</h2>
           <Pages />
         </div>
