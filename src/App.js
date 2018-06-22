adef540f8e63108094fb48391314ad04951c1983
Mateo Clarke <code@mateoclarke.com>
2018-06-22 15:23:17 -0500

    Add bootstrapv4
    Decided to go with bootstrap over USWDS bc its easy, they have good resets, and their grid system is already using flexbox. I want USWDS to be good, but I think Bootstrap has better docs and better features and utility classes in v4.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import PearlRoutes from './PearlRoutes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
